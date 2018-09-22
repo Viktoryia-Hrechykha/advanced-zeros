5b348e25f57bd38fdaf648d8cdb06f68751c5290
Viktoryia_Hrechykha <viktoryia_hrechykha@epam.com>
2018-09-23 01:54:58 +0300

    answer_advanced_zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var countZero = number;
+
+  for (let i = 2; i <= base; i++) {
+
+    if (base % i == 0) {
+
+      let exp = 0;
+
+      while (base % i == 0) {
+        base /=  i;
+        exp++;
+      }
+
+      let count = 0;
+      let tempNumber = number
+
+      while (tempNumber / i > 0) {
+        count = count + Math.floor(tempNumber / i);
+        tempNumber /= i;
+      }
+
+      if (countZero > Math.floor(count / exp)) countZero = Math.floor(count / exp);
+      
+    }
+
+  }
+
+  return countZero;
 }
